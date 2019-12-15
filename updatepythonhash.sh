36a5090e20fcb2da2d9f3a014e8e8e41760f541d
Teo <teodor.fratiloiu@gmail.com>
2019-12-15 12:36:11 +0100

    Updated hash key.

--- updatepythonhash.sh
+++ updatepythonhash.sh
@@ -2,7 +2,7 @@ python hashGen.py
 git commit -a -m "Updated hash key."
 git push
 echo Waiting for page to update.
-sleep 15
+sleep 30
 wget -O check.txt fratiloiu.com/key
 python hashCheck.py
 rm check.txt
