37acbe958883d9129d1f0f0f72a329a113521c23
Teo <teodor.fratiloiu@gmail.com>
2019-12-15 12:20:59 +0100

    Updated hash key.

--- hashCheck.py
+++ hashCheck.py
@@ -2,7 +2,7 @@ with open("check.txt","r") as textfile:
     content = textfile.readlines()
     hashToCheck = content[435:-49]
     print("Found hash on website: ", hashToCheck)
-with open("generatedHash.txt", r) as generatedHash:
+with open("generatedHash.txt", "r") as generatedHash:
     compare = generatedHash.readlines()
     if hashToCheck == compare:
         print("Hashes match.")
