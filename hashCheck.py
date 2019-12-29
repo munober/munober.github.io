3c0796e2ae5dca9aa07eaeab6fce19a66b7e42e6
Teo <teodor.fratiloiu@gmail.com>
2019-12-29 18:59:49 +0200

    Updated hash key.

--- hashCheck.py
+++ hashCheck.py
@@ -1,3 +1,13 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 with open("check.txt","r") as textfile:
     content = textfile.read()
     hashToCheck = content[436:-95]
@@ -6,6 +16,9 @@ with open("generatedHash.txt", "r") as generatedHash:
     compare = generatedHash.read()
     print("Hash to compare to:    ", compare)
     if hashToCheck == compare:
-        print("Hashes match.\033[92m")
+        # print("Hashes match.\033[92m")
+        print(f"{bcolors.OKGREEN}Hashes match.{bcolors.ENDC}")
     else:
-        print("Hashes dont yet match.\033[92m")
+        # print("Hashes dont yet match.\033[92m")
+        print(f"{bcolors.WARNING}Hashes dont yet match.{bcolors.ENDC}")
+
