ad3e9e0bef465d56b101001b247147018012f9ba
Teo G. Fratiloiu <teodor.fratiloiu@gmail.com>
2020-07-24 22:02:02 +0200

    Updated hash

--- updatepythonhash.sh
+++ updatepythonhash.sh
@@ -1,9 +1,9 @@
-python hashGen.py
+python3 hashGen.py
 git commit -a -m "Updated hash key."
 git push
 echo Waiting 30 seconds for page to update.
 sleep 30
 wget -O check.txt fratiloiu.com/key
-python hashCheck.py
+python3 hashCheck.py
 rm check.txt
 rm generatedHash.txt
